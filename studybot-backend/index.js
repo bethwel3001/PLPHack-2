2f976efbf6c8feab9ce44601d1b011c131a4c234
Bethwel Kiplagat <sjemnyango9@gmail.com>
2025-08-05 11:19:21 +0300

    CORS prod update

--- studybot-backend/index.js
+++ studybot-backend/index.js
@@ -6,9 +6,27 @@ const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// app.use(cors({
+//   origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+//   methods: ['GET', 'POST'],
+//   credentials: true
+// }));
+
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  'http://localhost:3000' 
+].filter(Boolean);
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-  methods: ['GET', 'POST'],
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'OPTIONS'],
   credentials: true
 }));
 app.use(express.json());
