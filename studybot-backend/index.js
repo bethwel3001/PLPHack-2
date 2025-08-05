f973bbb80544fc0e30e079682cca05a70802d5bc
Bethwel Kiplagat <sjemnyango9@gmail.com>
2025-08-05 18:43:16 +0300

    updated cors

--- studybot-backend/index.js
+++ studybot-backend/index.js
@@ -2,19 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
-
 const app = express();
 const port = process.env.PORT || 5000;
 
-// app.use(cors({
-//   origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-//   methods: ['GET', 'POST'],
-//   credentials: true
-// }));
-
 const allowedOrigins = [
   process.env.FRONTEND_URL,
-  'http://localhost:3000' 
+  'http://localhost:3000'
 ].filter(Boolean);
 
 app.use(cors({
