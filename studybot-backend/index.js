7e09335acc58310a755d4a363d25c1fab3bd4f60
Bethwel Kiplagat <sjemnyango9@gmail.com>
2025-08-05 19:36:46 +0300

    touched cors issues

--- studybot-backend/index.js
+++ studybot-backend/index.js
@@ -6,22 +6,25 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const allowedOrigins = [
-  process.env.FRONTEND_URL,
+  'https://sunny-basbousa-6ebe72.netlify.app',
   'http://localhost:3000'
-].filter(Boolean);
+];
 
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) !== -1) {
+    if (allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  methods: ['GET', 'POST', 'OPTIONS'],
+  methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
+// Handle preflight requests
+app.options('*', cors());
 app.use(express.json());
 
 const subjectPrompts = {
